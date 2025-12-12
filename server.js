fa7a7774750dee35368cca464d2643019096b360
Anil492-1011 <ayush121214@gmail.com>
2025-12-13 01:50:11 +0530

    Server Code

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ const app = express();
 const server = http.createServer(app);
 
 const allowedOrigins = [
-   "https://gravit-client.vercel.app/"
+   "https://gravit-client.vercel.app/",
+   "https://gravit-client-git-main-anil-daymas-projects.vercel.app/",
+   "https://gravit-client-j4h2z5j71-anil-daymas-projects.vercel.app/"
 ];
 
 app.use(cors({
